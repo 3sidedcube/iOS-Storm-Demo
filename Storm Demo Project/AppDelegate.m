537f8850889300f09fbcbcfce7e29046289ae428
Sam Houghton <sam@3sidedcube.com>
2014-11-06 13:16:39 +0000

    Adds native page

--- Storm Demo Project/AppDelegate.m
+++ Storm Demo Project/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "StormTheme.h"
+#import "SDPWelcomeViewController.h"
 @import ThunderCloud;
 
 @interface AppDelegate ()
@@ -18,16 +19,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    //Setup the shared theme
+    [TSCThemeManager setSharedTheme:[StormTheme new]];
+    
     //Setup the window
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     
-    self.window.rootViewController = [TSCAppViewController new];
+    self.window.rootViewController = [SDPWelcomeViewController new];
     
     [self.window makeKeyAndVisible];
-    
-    //Setup the shared theme
-    [TSCThemeManager setSharedTheme:[StormTheme new]];
     
     //Customise the app colours based on the values in the info.plist
     if (![[[NSBundle mainBundle] infoDictionary][@"TSCMainColor"] isEqualToString:@"000000"]) {
