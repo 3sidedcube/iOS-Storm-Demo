3abcdd5f080df489bd601bcb5a60ea7c5d0fdc50
Matthew Cheetham <matt@3sidedcube.com>
2018-02-15 09:41:16 +0000

    Upgrades project to latest version of storm and restores building

---
+++ Storm Demo Project/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Storm Demo Project
+//
+//  Created by Matthew Cheetham on 15/02/2018.
+//  Copyright © 2018 threesidedcube. All rights reserved.
+//
+
+import ThunderCloud
+
+@UIApplicationMain
+class AppDelegate: TSCAppDelegate {
+    
+}
